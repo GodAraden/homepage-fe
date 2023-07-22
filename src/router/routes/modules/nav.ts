d389e6e89ad178fcb5656a0176ca55600d0be75c
Araden <2713886816@qq.com>
2023-07-22 13:16:40 +0800

    fix: 修正 Nav 路由路径大小写

--- src/router/routes/modules/nav.ts
+++ src/router/routes/modules/nav.ts
@@ -3,7 +3,7 @@ import { AppRouteRecordRaw } from '../types'
 const NAV: AppRouteRecordRaw = {
   path: '/nav',
   name: 'Nav',
-  component: () => import('@/view/Nav/index.vue'),
+  component: () => import('@/view/nav/index.vue'),
   meta: {
     requiresAuth: false,
     icon: 'icon-Nav',
