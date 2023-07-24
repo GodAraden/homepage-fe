0efed97f2f5425d37328d886febd604d46e27120
Araden <2713886816@qq.com>
2023-07-24 19:10:02 +0800

    style: 移除了使用 arco 做的导航栏，太难用

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,53 +1,30 @@
 <template>
-  <div class="h-16 flex backdrop-blur">
-    <a href="/" class="flex items-center w-64">
+  <div class="h-16 flex backdrop-blur bg-slate-700 bg-opacity-20 shadow-md">
+    <a href="/" class="flex items-center w-96">
       <img src="/logo.svg" alt="" class="h-12" />
       <a-typography-title
         :heading="4"
-        class="mb-0 ml-2 text-gray-200"
+        class="!mb-0 ml-2"
         style="font-family: DaKai_For_Title"
       >
         {{ $t(`header.title`) }}
       </a-typography-title>
     </a>
-    <a-menu
-      mode="horizontal"
-      :default-selected-key="['/']"
-      class="flex flex-1 items-center justify-center"
-    >
-      <router-link
+
+    <ul class="flex flex-1 justify-between items-center text-lg font-black">
+      <li
         v-for="item in appRoutes.filter((route) => !route.meta.requiresAuth)"
         :key="item.path"
-        :to="item.path"
       >
-        <a-menu-item
-          :key="item.path"
-          class="mr-2 font-black hover:bg-transparent navbar-btn"
-        >
+        <router-link :to="item.path">
           <icon-font :type="item.meta.icon" />
-          <span class="-ml-2"> {{ $t(`header.navbar.${item.name}`) }} </span>
-        </a-menu-item>
-      </router-link>
-    </a-menu>
-    <div class="flex w-64 items-center justify-end">
-      <a-dropdown :popup-max-height="false" position="br" class="w-96">
-        <a-button type="outline" size="large" shape="circle">
-          <template #icon><icon-user /><icon-down :size="12" /></template>
-        </a-button>
-        <template #content>
-          <a-form :model="form" class="user-panel">
-            <a-form-item field="name" label="Username">
-              <a-input />
-            </a-form-item>
-            <a-form-item field="post" label="Post">
-              <a-input />
-            </a-form-item>
-            <a-form-item>
-              <a-button html-type="submit">Submit</a-button>
-            </a-form-item>
-          </a-form>
-        </template>
-      </a-dropdown>
+          <span class="ml-1"> {{ $t(`header.navbar.${item.name}`) }} </span>
+        </router-link>
+      </li>
+    </ul>
+
+    <div class="flex w-96 items-center justify-end">
+      <icon-user /><icon-down :size="12" />
     </div>
   </div>
 </template>
@@ -55,14 +32,8 @@
 <script setup lang="ts">
 import { Icon } from '@arco-design/web-vue'
 import { appRoutes } from '@/router/routes'
-import { reactive } from 'vue'
 
 const IconFont = Icon.addFromIconFontCn({ src: import.meta.env.VITE_ALI_CDN })
-
-const form = reactive({
-  name: '',
-  post: ''
-})
 </script>
 
 <style lang="less" scoped>
@@ -70,15 +41,5 @@ const form = reactive({
   &:hover {
     color: rgb(var(--arcoblue-6));
   }
-}
-
-:deep(.arco-menu-overflow-wrap) {
-  text-align: center;
-}
-
-:deep(.arco-dropdown) {
-  background-color: transparent !important;
-  border: none;
-  background-color: rgba(0, 0, 0, 0.14);
 }
 </style>
