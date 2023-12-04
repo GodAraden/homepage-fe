fd3bbf255968ca1b487956b3a59cd8527c44001d
Araden <2713886816@qq.com>
2023-12-04 21:30:45 +0800

    friendLink: 添加 猫猫虫的后花园

--- src/config/friendLink.ts
+++ src/config/friendLink.ts
@@ -5,5 +5,12 @@ export const FriendLinks: FriendLink[] = [
     logo: '/avatar.jpg',
     link: import.meta.env.VITE_APP_OWNER_LINK,
     color: import.meta.env.VITE_APP_OWNER_COLOR
+  },
+  {
+    title: '猫猫虫的后花园',
+    description: '一个亦正亦邪的猫猫虫的个人网站',
+    logo: 'https://raw.githubusercontent.com/mlhiter/typora-images/master/blog-picture.jpg',
+    link: 'https://www.mlhiter.top/',
+    color: 'rgba(255, 255, 255, 0.1)'
   }
 ]
